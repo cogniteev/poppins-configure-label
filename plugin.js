495dfbf40abdbad9db97879bd60c2b203ae7b998
Anthony Pessy <anthony@cogniteev.com>
2016-04-07 11:00:50 +0200

    only apply the status/0-triage tag on pull request
    also, the callback is only called on 'opened' event so no need to verify state

--- plugin.js
+++ plugin.js
@@ -1,18 +1,8 @@
 module.exports = function (poppins) {
 
-  function isOpen(event) {
-    return event.issue && event.issue.state === 'open';
-  }
-
-  function isStatusSet(event) {
-    return event.issue.labels.reduce(function(hasStatus, label) {
-      return hasStatus || label.startsWith('status/');
-    }, false);
-  }
-
   poppins.plugins.labels = {
     'status/0-triage': function (event) {
-      return isOpen(event) && !isStatusSet(event);
+      return event.pull_request !== undefined;
     }
   }
 
