7ffcb4d43c59237f0906122f565863e2c45c8cf9
Anthony Pessy <anthony@cogniteev.com>
2016-04-07 10:08:55 +0200

    fix check and use functions for easy reuse and better readability

--- plugin.js
+++ plugin.js
@@ -1,15 +1,19 @@
 module.exports = function (poppins) {
 
-  poppins.labels = {
-
-    'status/0-triage': function (issue) {
-      if (issue.state === 'open') {
-        return issue.labels.reduce(function(hasStatus, label) {
-          return hasStatus || label.startsWith('status/');
-        }, false);
-      }
-      return false;
-    }
-
+  function isOpen(event) {
+    return event.issue && event.issue.state === 'open';
   }
+
+  function isStatusSet(event) {
+    return event.issue.labels.reduce(function(hasStatus, label) {
+      return hasStatus || label.startsWith('status/');
+    }, false);
+  }
+
+  poppins.plugins.labels = {
+    'status/0-triage': function (event) {
+      return isOpen(event) && !isStatusSet(event);
+    }
+  }
+
 };
