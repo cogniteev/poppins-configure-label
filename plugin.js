1e2ec0aaa5febac8adc9a2181a31a88e51b122f9
Anthony Pessy <anthony.pessy@gmail.com>
2016-04-11 11:11:59 +0200

    do not rely on poppins-label plugin anymore because it assumes labels is part of the pull request whereas it is not

--- plugin.js
+++ plugin.js
@@ -1,9 +1,32 @@
 module.exports = function (poppins) {
 
-  poppins.plugins.labels = {
-    'status/0-triage': function (event) {
-      return event.pull_request !== undefined;
-    }
+  function setLabels(number, labels) {
+    poppins.rest.issues.edit({
+      user    : poppins.config.msg.user,
+      repo    : poppins.config.msg.repo,
+      number  : number,
+      labels  : labels
+    }).catch(poppins.log.bind(poppins));
   }
+
+  function hasStatusLabel(labels) {
+    return labels.find(function(label) {
+      return label.name.startsWith('status/');
+    }) !== undefined;
+  }
+
+  poppins.on('pullRequestOpened', function(data) {
+    poppins.getIssueLabels(data.number)
+        .then(function(labels) {
+          labels = labels.map(function(item) {
+            return item.name;
+          });
+
+          if (!hasStatusLabel(labels)) {
+            labels.push('status/0-triage');
+          }
+          setLabels(data.number, labels);
+        });
+  });
 
 };
